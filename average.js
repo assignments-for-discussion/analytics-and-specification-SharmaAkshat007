d497de1c136da23e15af92a13e51e30430447d23
Akshat Sharma <akshatsharma992001@gmail.com>
2021-12-09 19:53:20 +0530

    first

--- average.js
+++ average.js
@@ -1,6 +1,8 @@
-
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  if (numbers.length == 0) {
+    return NaN;
+  }
+  return numbers.reduce((p, c) => p + c, 0) / numbers.length;
 }
 
-module.exports = {average};
+module.exports = { average };
