2fac82edb5cdb953651b4449e34b43d257adf193
Akshat Sharma <akshatsharma992001@gmail.com>
2021-12-15 15:55:06 +0530

    separate loops for summing and elimination of NaNs

--- average.js
+++ average.js
@@ -1,16 +1,21 @@
+const checkNan = (numbers) => {
+  numbers = numbers.filter((number) => !isNaN(number));
+
+  return [numbers, numbers.length];
+};
+
 function average(numbers) {
   let size = numbers.length;
 
-  if (size == 0) {
+  if (size === 0) {
     return NaN;
   }
 
+  [numbers, size] = checkNan(numbers);
+
   return (
     numbers.reduce((p, c) => {
-      if (isNaN(c)) {
-        size -= 1;
-        return p;
-      } else if (typeof c === "string") {
+      if (typeof c === "string") {
         return p + Number(c);
       }
       return p + c;
